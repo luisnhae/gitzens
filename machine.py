aa41fbb24bea1ab8d3860ba9411e7994ac63009c
Luis Antunes <luis.nhae@outlook.com>
2020-03-20 20:23:02 -0300

    Algoritmo programinha tempo de vida

---
+++ machine.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+print("")
+print("Quanto tempo de vida você tem?\n")
+g = int(input('How old? >>> '))
+d = 24
+y = 365
+h = ((d*y)*g)
+ws = 3600
+m = h*ws/60
+dds = ws*d
+yys = y*dds
+s = yys*g
+print("")
+print("you're alive\n")
+print("hours: ",h)
+print("minutes: ",m)
+print("seconds: ",s)
